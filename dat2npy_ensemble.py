c2417216b5a772d2771bf312c1d7396aebefb225
jacob975 <z123a123s123@gmail.com>
2018-05-31 23:08:22 +0800

    Collect all programs about dat2npy in the ensemble version

--- dat2npy_ensemble.py
+++ dat2npy_ensemble.py
@@ -2,18 +2,8 @@
 '''
 Abstract:
     This is a program to convert .dat files to npy files
-    The feature of this version is that set all number smaller than 0 as 0
-
---------------------------------------------------
-                no observation      no detection
---------------------------------------------------
-dat2npy         0.00                0.00
-dat2npy_const   -9.99e+02           -9.99e+02
-dat2npy_pair    -9.99e+02           0.00
---------------------------------------------------
-
 Usage:
-    dat2npy_1_r.py [file name 1] [file name 2] [file name 3] ...
+    dat2npy.py [file name 1] [file name 2] [file name 3] ...
     [file name]:
         The file you want to processed.
         The first will be labeded as 0, the second will be labeded as 1, so as on.
@@ -29,48 +19,46 @@ Example:
 Editor:
     Jacob975
 
-20180530
+20180123
 ####################################
 update log
+20180123 version alpha 1
+    Now it works, the code can convert both source and label into tensorflow readable
+20180124 version alpha 2
+    1. Now feature, you can choose processing label or data by argv.
+    2. Now the data will be normalized.
+20180301 version alpha 3
+    1. You can choose how many zero will be tolerated.
+20180306 version alpha 4
+    1. no argv for data mod and label mod anymore, for replacement, the code will generate label with data process.
+    2. now you can process a sequence of data with label in order.
+20180320 version alpha 5 
+    1. add a tracer to dat data set
+20180322 version alpha 6
+    1. rename tracer
+20180323 version alpha 7:
+    1. rearrange the tracer
+20180414 version alpha 8:
+    1. denote no-observation as -9.99+e02
+    2. rename func nozero_filter as no_observation_filter
+20180415 version alpha 9:
+    1. denote no-observation as 0 instead of -9.99+e02, if you need -9.99e+02, please use dat2npy_const.py
+20180430 version alpha 10:
+    1. This program become a prototype
+    2. the default setting of no observation and no detections is 0, 0
+20180530 version alpha 11:
+    1. add a new func to save coord infomations
+20180531 version alpha 12:
+    1. all dat2npy programs are collected into the ensemble version
 '''
-import tensorflow as tf
 import time
 import re           # this is used to apply multiple spliting
 import numpy as np
 from sys import argv
-from dat2npy_noobs_nodet import read_well_known_data
+from dat2npy_lib import read_well_known_data, apply_filter_on
 
 # how many element in a data vector
 data_width = 16
-
-def normalize(inp):
-    # take norm
-    h = len(inp)
-    norm = np.amax(inp, axis=1)
-    outp = inp / norm.reshape(h,1)
-    # make each no observation having the same value
-    outp[inp == -9.99e+02] = -1.0
-    outp[inp == 0.0] = -1.0
-    outp.reshape(-1, data_width)
-    return outp
-
-def no_observation_filter(inp, maximun):
-    # set up MaxLoss filter
-    _filter= np.array([ np.count_nonzero(row == -1.0) <= maximun for row in inp])
-    # apply filter
-    outp = inp[_filter]
-    outp.reshape(-1, data_width)
-    return outp, _filter
-
-# This code is used to apply filter on certain files
-def apply_filter_on(name_file, _filter):
-    try:
-        inp = np.loadtxt(name_file)
-    except:
-        print ("No such file or directory: {0}".format(name_file))
-        return 1, None
-    outp = inp[_filter]
-    return 0, outp
 
 #--------------------------------------------
 # main code
@@ -78,19 +66,57 @@ if __name__ == "__main__":
     VERBOSE = 0
     # measure times
     start_time = time.time()
+    # initialize
+    options = ['0_r', '0_r_noH', '0_r_noMIPS', '0_r_noH_noMIPS', '0_r_noJHK', '1_1', '1_0', '1_r', '0_0']
     #----------------------------------
     # check argv is right
-    if len(argv) == 1:
-        print ("Error!\nUsage: dat2npy_1_r.py [dat files]")
+    if len(argv) < 3:
+        print ("Error!\nUsage: dat2npy_ensemble.py [mod] [dat files]")
+        print("possible mods: ")
+        print(", ".join('%s'%x for x in options))
         exit()
     # read argv
-    data_name_list = argv[1:]
+    # Base on mod, loading corresponding functions
+    mod = argv[1]
+    if mod == '0_r':
+        from dat2npy_lib import normalize_0_r as normalize
+        from dat2npy_lib import no_observation_filter_eq_0 as no_observation_filter
+    elif mod == '0_r_noH_noMIPS':
+        from dat2npy_lib import normalize_0_r_noH_noMIPS as normalize
+        from dat2npy_lib import no_observation_filter_eq_0 as no_observation_filter
+    elif mod == '0_r_noH':
+        from dat2npy_lib import normalize_0_r_noH as normalize
+        from dat2npy_lib import no_observation_filter_eq_0 as no_observation_filter
+    elif mod == '0_r_noMIPS':
+        from dat2npy_lib import normalize_0_r_noMIPS as normalize
+        from dat2npy_lib import no_observation_filter_eq_0 as no_observation_filter
+    elif mod == '0_r_noJHK':
+        from dat2npy_lib import normalize_0_r_noJHK as normalize
+        from dat2npy_lib import no_observation_filter_eq_0 as no_observation_filter
+    elif mod == '1_1':
+        from dat2npy_lib import normalize_1_1 as normalize
+        from dat2npy_lib import no_observation_filter_smaller_than_or_eq_minus1 as no_observation_filter
+    elif mod == '1_r':
+        from dat2npy_lib import normalize_1_r as normalize
+        from dat2npy_lib import no_observation_filter_eq_minus1 as no_observation_filter
+    elif mod == '1_0':
+        from dat2npy_lib import normalize_1_0 as normalize
+        from dat2npy_lib import no_observation_filter_eq_minus1 as no_observation_filter
+    elif mod == '0_0':
+        from dat2npy_lib import normalize_0_0 as normalize
+        from dat2npy_lib import no_observation_filter_smaller_than_or_eq_0 as no_observation_filter
+    else:
+        print("Wrong mod, program stops.")
+        print("possible mods: ")
+        print(", ".join('%s'%x for x in options))
+        exit()
+    data_name_list = argv[2:]
     print ("The command is:\n {0}".format(argv))
     print ("data to be processed: {0}".format(data_name_list))
     # how many element in a data vector
     data_width = 16
     #-----------------------------------
-    # Load data
+    # Load data, label, tracer, and coordinate
     sum_data = [[] for x in range(data_width)]
     sum_label = [[] for x in range(data_width)]
     sum_tracer = [[] for x in range(data_width)]
@@ -104,8 +130,9 @@ if __name__ == "__main__":
         data = np.array(str_data, dtype = float)
         data_n = normalize(data)
         # no observation filter
+        # i is the tolarence of loss in a single datum
         for i in range(data_width):
-            data_n_z, _filter = no_observation_filter(data_n, i)
+            data_n_z, _filter= no_observation_filter(data_n, i)
             name_tracer = "{0}_tracer.dat".format(data_name[:4])
             name_coord = "{0}_coord.dat".format(data_name[:4])
             failure, tracer_outp = apply_filter_on(name_tracer, _filter)
@@ -115,7 +142,7 @@ if __name__ == "__main__":
             label_z_f = [[0 for k in range(3)] for j in range(len(label_z))]
             for u in range(len(label_z_f)):
                 label_z_f[u][int(label_z[u])] = 1
-            #-------------------------------------------------------------
+            #-----------------------------------------------------
             # stack them
             sum_data[i] = np.append(sum_data[i], data_n_z)
             sum_label[i] = np.append(sum_label[i], label_z_f)
@@ -125,11 +152,12 @@ if __name__ == "__main__":
                 sum_coord[i] = np.append(sum_coord[i], coord_outp)
             elif failure:
                 sum_coord[i] = np.append(sum_coord[i], np.zeros(2* len(_filter)))
-            #-------------------------------------------------------------
-    # save data
+            #-----------------------------------------------------
+    # save data, label, tracer, and coordinate
     print ("###############################")
-    print ("save data, label, and tracer")
+    print ("save data, label, tracer, and coordinate")
     for i in range(data_width):
+        # reshape the data because np.append smooth the array.
         sum_data[i] = np.reshape(sum_data[i], (-1, data_width))
         sum_label[i] = np.reshape(sum_label[i], (-1, 3))
         sum_coord[i] = np.reshape(sum_coord[i], (-1, 2))
---
+++ dat2npy_lib.py
@@ -0,0 +1,204 @@
+#!/usr/bin/python3
+'''
+Abstract:
+    This is a library to provide functions for converting .dat files to npy files
+Editor:
+    Jacob975
+
+2018531
+####################################
+update log
+20180531 version alpha 1
+    1. This is the library of dat2npy program in ensemble version 
+'''
+import time
+import re           # this is used to apply multiple spliting
+import numpy as np
+from sys import argv
+
+# how many element in a data vector
+data_width = 16
+
+# the def is used to read a list of data with the same class.
+def read_well_known_data(data_name):
+    if data_name[-3:] == "npy":
+        return np.load(data_name)
+    elif data_name[-3:] == "txt":
+        return np.loadtxt(data_name)
+    f = open(data_name, 'r')
+    data = []
+    for ind, line in enumerate(f.readlines(), start = 0):
+        # skip if no data or it's a hint.
+        if not len(line) or line.startswith('#'):
+            continue
+        row = re.split('[,\n\[\]]+', line)
+        # clean the empty element
+        row_c = [x for x in row if x != ""]
+        if len(row_c) != data_width:
+            print (line[:-1])
+            print ("the row {0} is wrong.\n".format(ind))
+            continue
+        data.append(row_c)
+    f.close()
+    return data
+
+#---------------------------------------------------
+def normalize_0_0(inp):
+    # take norm
+    h = len(inp)
+    norm = np.amax(inp, axis=1)
+    outp = inp / norm.reshape(h,1)
+    # make each no observation having the same value
+    outp[inp == -9.99e+02 ] = 0.0
+    outp[inp == -0.0 ] = 0.0
+    outp.reshape(-1, data_width)
+    return outp
+
+def normalize_1_1(inp):
+    # take norm
+    h = len(inp)
+    norm = np.amax(inp, axis=1)
+    outp = inp / norm.reshape(h,1)
+    # make each no observation having the same value
+    outp[outp <= 0.0] = -1.0
+    outp.reshape(-1, data_width)
+    return outp
+
+def normalize_1_r(inp):
+    # take norm
+    h = len(inp)
+    norm = np.amax(inp, axis=1)
+    outp = inp / norm.reshape(h,1)
+    # make each no observation having the same value
+    outp[inp == -9.99e+02] = -1.0
+    outp[inp == 0.0] = -1.0
+    outp.reshape(-1, data_width)
+    return outp
+
+def normalize_1_0(inp):
+    # take norm
+    h = len(inp)
+    norm = np.amax(inp, axis=1)
+    outp = inp / norm.reshape(h,1)
+    # make each no observation having the same value
+    outp[outp <= 0.0] = 0.0
+    for row in outp:
+        for i in range(len(row)//2):
+            if (row[i] <= 0.0) and (row[i+8] <= 0.0):
+                row[i] = row[i+8] = -1.0
+    outp.reshape(-1, data_width)
+    return outp
+
+def normalize_0_r(inp):
+    # take norm
+    h = len(inp)
+    norm = np.amax(inp, axis=1)
+    outp = inp / norm.reshape(h,1)
+    # make each no observation having the same value
+    outp[inp == -9.99e+02] = 0.0
+    outp[inp == 0.0] = 0.0
+    outp.reshape(-1, data_width)
+    return outp
+
+def normalize_0_r_noH(inp):
+    # take norm
+    h = len(inp)
+    # remove band H
+    inp[:,1] = 0.0
+    inp[:,9] = 0.0
+    norm = np.amax(inp, axis=1)
+    outp = inp / norm.reshape(h,1)
+    # make each no observation having the same value
+    outp[inp == -9.99e+02] = 0.0
+    outp[inp == 0.0] = 0.0
+    outp.reshape(-1, data_width)
+    return outp
+    
+def normalize_0_r_noMIPS(inp):
+    # take norm
+    h = len(inp)
+    # remove band MIPS 24 um
+    inp[:,7] = 0.0
+    inp[:,15] = 0.0
+    norm = np.amax(inp, axis=1)
+    outp = inp / norm.reshape(h,1)
+    # make each no observation having the same value
+    outp[inp == -9.99e+02] = 0.0
+    outp[inp == 0.0] = 0.0
+    outp.reshape(-1, data_width)
+    return outp
+
+def normalize_0_r_noH_noMIPS(inp):
+    # take norm
+    h = len(inp)
+    # remove band H
+    inp[:,1] = 0.0
+    inp[:,9] = 0.0
+    # remove band MIPS 24 um
+    inp[:,7] = 0.0
+    inp[:,15] = 0.0
+    norm = np.amax(inp, axis=1)
+    outp = inp / norm.reshape(h,1)
+    # make each no observation having the same value
+    outp[inp == -9.99e+02] = 0.0
+    outp[inp == 0.0] = 0.0
+    outp.reshape(-1, data_width)
+    return outp
+
+def normalize_0_r_noJHK(inp):
+    # take norm
+    h = len(inp)
+    # remove band JHK
+    inp[:,0:3] = 0.0
+    inp[:,8:11] = 0.0
+    norm = np.amax(inp, axis=1)
+    outp = inp / norm.reshape(h,1)
+    # make each no observation having the same value
+    outp[inp == -9.99e+02] = 0.0
+    outp[inp == 0.0] = 0.0
+    outp.reshape(-1, data_width)
+    return outp
+
+#------------------------------------------------------
+def no_observation_filter_smaller_than_or_eq_0(inp, maximun):
+    # set up MaxLoss filter
+    _filter= np.array([ np.count_nonzero(row <= 0.0) <= maximun for row in inp])
+    # apply filter
+    outp = inp[_filter]
+    outp.reshape(-1, data_width)
+    return outp, _filter
+
+def no_observation_filter_eq_0(inp, maximun):
+    # set up MaxLoss filter
+    _filter= np.array([ np.count_nonzero(row == 0.0) <= maximun for row in inp])
+    # apply filter
+    outp = inp[_filter]
+    outp.reshape(-1, data_width)
+    return outp, _filter
+
+def no_observation_filter_smaller_than_or_eq_minus1(inp, maximun):
+    # set up MaxLoss filter
+    _filter= np.array([ np.count_nonzero(row <= -1.0) <= maximun for row in inp])
+    # apply filter
+    outp = inp[_filter]
+    outp.reshape(-1, data_width)
+    return outp, _filter
+
+def no_observation_filter_eq_minus1(inp, maximun):
+    # set up MaxLoss filter
+    _filter= np.array([ np.count_nonzero(row == -1.0) <= maximun for row in inp])
+    # apply filter
+    outp = inp[_filter]
+    outp.reshape(-1, data_width)
+    return outp, _filter
+#------------------------------------------------------
+
+# This code is used to apply filter on certain files
+def apply_filter_on(name_file, _filter):
+    try:
+        inp = np.loadtxt(name_file)
+    except:
+        print ("No such file or directory: {0}".format(name_file))
+        return 1, None
+    outp = inp[_filter]
+    return 0, outp
