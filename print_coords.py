d88feddb85fd42e1245e54020fd1169a54f28fda
jacob975 <z123a123s123@gmail.com>
2018-06-05 23:30:43 +0800

    Add a func to print coordinates of high reliable data only

--- print_coords.py
+++ print_coords.py
@@ -17,6 +17,8 @@ Editor:
 update log
 20180529 version alpha 1
     1. the code is under testing
+20180605 version alpha 2
+    1. Add a func to print the coordinates of high reliable data only.
 '''
 import time
 import numpy as np
@@ -33,19 +35,16 @@ if __name__ == "__main__":
     # Load argv
     if len(argv) != 5:
         print ("Error!\nUsage:\tprint_coords.py [DIR where AI saved] [keyword] [true_label] [pred_label]")
-        print ("Example:\tprint_coords.py . MaxLoss15 1 1")
+        print ("Example:print_coords.py . MaxLoss15 1 1")
         exit()
     directory = argv[1]
     keyword = argv[2]
     true_label = int(argv[3])
     pred_label = int(argv[4])
     # Load files
-    failure, data, tracer = load_lib.load_arrangement(keyword, directory)
+    failure, labels_pred = load_lib.load_labels_pred(keyword, directory)
     if not failure:
-        print ("load data and tracer success")
-    failure, cls_pred = load_lib.load_cls_pred(keyword, directory)
-    if not failure:
-        print ("load cls_pred success")
+        print ("load labels_pred success")
     failure, cls_true = load_lib.load_cls_true(keyword, directory)
     if not failure:
         print ("load cls_true success")
@@ -53,12 +52,15 @@ if __name__ == "__main__":
     failure, coords = load_lib.load_coords(keyword, directory)
     if not failure:
         print ("load coordinates success")
-    failure, cm = load_lib.confusion_matrix(cls_true, cls_pred)
+    infos = load_lib.confusion_matrix_infos(cls_true, labels_pred)
+    failure, cm = load_lib.confusion_matrix(cls_true, infos.cls_pred)
     print(cm)
     # read the coords of data in certain position of confusion matrix
-    index_of_certain_data = np.where((cls_true == true_label) & (cls_pred == pred_label))
+    index_of_certain_data = np.where((cls_true == true_label) & (infos.cls_pred == pred_label))
     coords_of_certain_data = coords.test[index_of_certain_data]
-    # print coords out
+    index_of_high_reliable_certain_data = np.where((cls_true == true_label) & (infos.cls_pred == pred_label) & (np.max(infos.labels_pred, axis = 1) > 0.8))
+    coords_of_high_reliable_certain_data = coords.test[index_of_high_reliable_certain_data]
+    # print coords out to test if the list correct.
     print("number of coords of selected sources: {0}".format(len(coords_of_certain_data)))
     print("5 examples")
     for i in range(5):
@@ -66,10 +68,18 @@ if __name__ == "__main__":
             print("{0} {1}".format(coords_of_certain_data[i][0], coords_of_certain_data[i][1]))
         except:
             break
-    print("...")
+    print("...\n")
+    print("number of coords of selected high reliable sources: {0}".format(len(coords_of_high_reliable_certain_data)))
+    print("5 examples")
+    for i in range(5):
+        try:
+            print("{0} {1}".format(coords_of_high_reliable_certain_data[i][0], coords_of_high_reliable_certain_data[i][1]))
+        except:
+            break
+    print("...\n")
     # save the result
-    np.save("{0}/coords_test_{1}_{2}.npy".format(directory, true_label, pred_label), coords_of_certain_data)
     np.savetxt("{0}/coords_test_{1}_{2}.txt".format(directory, true_label, pred_label), coords_of_certain_data)
+    np.savetxt("{0}/coords_test_HR_{1}_{2}.txt".format(directory, true_label, pred_label), coords_of_high_reliable_certain_data)
     #-----------------------------------
     # measure time
     elapsed_time = time.time() - start_time
--- print_coords.sh
+++ print_coords.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-
+# The program is used to generate a list of coordinates of certain sources.
+# In 3x3 SCAO confusion matrix
+# The program create a list for each element contains coordinates of data belonged that element.
 # Usage: print_coords.sh [working directory] [key word]
 
 # 20180531 version alpha 1
 # The code work
+# 20180605 version alpha 2
+# Add a func to print coordinates of high reliable sources only.
 
 if [ "$#" -ne 2 ]; then
     echo "Illegal number of parameters"
@@ -36,6 +40,8 @@ while [ $_true -le 2 ]
 do
     cat coords_test_${_true}_0.txt coords_test_${_true}_1.txt coords_test_${_true}_2.txt \
     > coords_test_true_${_true}.txt
+    cat coords_test_HR_${_true}_0.txt coords_test_HR_${_true}_1.txt coords_test_HR_${_true}_2.txt \
+    > coords_test_true_HR_${_true}.txt
     ((_true++))
 done
 pred=0
@@ -43,13 +49,20 @@ while [ $pred -le 2 ]
 do
     cat coords_test_0_${pred}.txt coords_test_1_${pred}.txt coords_test_2_${pred}.txt \
     > coords_test_pred_${pred}.txt
+    cat coords_test_HR_0_${pred}.txt coords_test_HR_1_${pred}.txt coords_test_HR_2_${pred}.txt \
+    > coords_test_HR_pred_${pred}.txt
     ((pred++))
 done
 # print coordinates of diagonal terms and non-diagonal terms
 # diagonal terms
+cat coords_test_HR_0_0.txt coords_test_HR_1_1.txt coords_test_HR_2_2.txt \
+    > coords_test_HR_diag.txt
 cat coords_test_0_0.txt coords_test_1_1.txt coords_test_2_2.txt \
     > coords_test_diag.txt
 # non-diagonal terms
 cat coords_test_0_1.txt coords_test_0_2.txt coords_test_1_0.txt \
     coords_test_1_2.txt coords_test_2_0.txt coords_test_2_1.txt \
     > coords_test_nondiag.txt
+cat coords_test_HR_0_1.txt coords_test_HR_0_2.txt coords_test_HR_1_0.txt \
+    coords_test_HR_1_2.txt coords_test_HR_2_0.txt coords_test_HR_2_1.txt \
+    > coords_test_HR_nondiag.txt
