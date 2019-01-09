3e34deba01aeca34cd04346a5495dcdffa6bb1e4
jacob975 <z123a123s123@gmail.com>
2019-01-09 14:32:24 +0800

    Add new functions for excluding upper limit.

---
+++ exclude_ul.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+'''
+Abstract:
+    This is a program to exclude the upper limit happened in band 1 ~ 7 
+Usage:
+    excluded_ul.py [upper limit table] [data]
+    
+    Input should looks like:
+    upper limit table = 
+    [[ A, A, A, A, A, A, A, U],
+     [ A, B, A, A, A, A, A, U],
+     ...]
+
+Editor:
+    Jacob975
+
+##################################
+#   Python3                      #
+#   This code is made in python3 #
+##################################
+
+20181128
+####################################
+update log
+20181128 version alpha 1:
+    1. The code works
+'''
+from sys import argv
+import time
+import numpy as np
+
+#--------------------------------------------
+# main code
+if __name__ == "__main__":
+    VERBOSE = 0
+    # measure time
+    start_time = time.time()
+    #-----------------------------------
+    # Load argv
+    if len(argv) != 3:
+        print ("Wrong numbers of arguments")
+        print ("Usage: mask_ul.py [upper limit table] [data]")
+        exit(0)
+    ul_table_name = argv[1]
+    data_name = argv[2]
+    #-----------------------------------
+    # Load data
+    ul_table = np.loadtxt(ul_table_name, dtype = str)
+    data = np.loadtxt(data_name, dtype = str)
+    #------------------------------------------
+    # Find the line with upper limits
+    data_no_ul = data[  (ul_table[:,0] != 'U') &\
+                        (ul_table[:,1] != 'U') &\
+                        (ul_table[:,2] != 'U') &\
+                        (ul_table[:,3] != 'U') &\
+                        (ul_table[:,4] != 'U') &\
+                        (ul_table[:,5] != 'U') &\
+                        (ul_table[:,6] != 'U')]
+    # Save masked data set
+    np.savetxt("{0}_tmp.txt".format(data_name[:-4]), data_no_ul, fmt = '%s')
+    #-----------------------------------
+    # measure time
+    elapsed_time = time.time() - start_time
+    print ("Exiting Main Program, spending ", elapsed_time, "seconds.")
---
+++ rand_arrange.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+'''
+Abstract:
+    This is a program randomly arranging the train, validate, and test dataset.
+Usage:
+    random_arrange.py [train size] [validate size] [test size]
+Editor:
+    Jacob975
+
+##################################
+#   Python3                      #
+#   This code is made in python3 #
+##################################
+
+20190109
+####################################
+update log
+'''
+import time
+from sys import argv
+import numpy as np
+
+#--------------------------------------------
+# Main code
+if __name__ == "__main__":
+    VERBOSE = 0
+    # Measure time
+    start_time = time.time()
+    #-----------------------------------
+    # Load argv
+    if len(argv) != 5:
+        print ("The number of arguments is wrong.")
+        print ("Usage: rand_arrange.py [# of data] [train size] [validate size] [test size]")
+        exit()
+    num_data = int(argv[1])
+    train_weight = int(argv[2])
+    validate_weight = int(argv[3])
+    test_weight = int(argv[4])
+    #-----------------------------------
+    # Make a random seed and apply on data
+    randomize = np.arange(num_data)
+    np.random.shuffle(randomize)
+    total_weight = train_weight + validate_weight + test_weight
+    # Derive the size from the weight of dataset and the number of data.
+    train_size = int(num_data * train_weight/total_weight)
+    validate_size = int(num_data * validate_weight/total_weight)
+    test_size = int(num_data * test_weight/total_weight)
+    print ("Type 'exit' to close the program.")
+    # Apply on dataset iteratively.
+    while True:
+        file_name = input("Please specify the file you want to do random arrangements:\n")
+        if file_name == "exit":
+            exit()
+        tmp_file = np.loadtxt(file_name, dtype = str)
+        tmp_file = tmp_file[randomize]
+        file_for_train = tmp_file[:train_size] 
+        file_for_validate = tmp_file[train_size:validate_size + train_size]
+        file_for_test = tmp_file[validate_size + train_size:]
+        # Save the data
+        np.savetxt("train_{0}".format(file_name),    file_for_train, fmt = '%s')
+        np.savetxt("validate_{0}".format(file_name), file_for_validate, fmt = '%s')
+        np.savetxt("test_{0}".format(file_name),     file_for_test, fmt = '%s')
+        print("Done.")
+    #-----------------------------------
+    # Measure time
+    elapsed_time = time.time() - start_time
+    print("Exiting Main Program, spending ", elapsed_time, "seconds.")
