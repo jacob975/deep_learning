9a67dbf42a3092f52f28ef9cff6bd1e134028729
jacob975 <z123a123s123@gmail.com>
2019-07-31 14:07:40 +0800

    Minor fix

--- mask_ul.py
+++ mask_ul.py
@@ -33,7 +33,7 @@ import numpy as np
 def proper_error(error_list):
     # Sort the list
     error_array = np.sort(error_list)
-    # Take the index of the 20% position.
+    # Take the index of the 10% position.
     index = int(len(error_array) * 0.1)
     error = np.median(error_array[:index])
     return error
